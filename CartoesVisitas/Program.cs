99bab33f24e5cb12f0813caed235a546bb7fa328
Thamires <thamy.03@hotmail.com>
2023-10-24 21:26:45 -0300

    Primeiro commit

---
+++ CartoesVisitas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+// Cria as variáveis para armazenar os valores
+string? nome;
+string? profissao;
+string? telefone;
+string? email;
+string? descricao;
+
+// Solicita as informações do cartão
+Console.WriteLine("Digite o seu  nome");
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite sua profissão?");
+profissao = Console.ReadLine();
+
+Console.WriteLine("Digite seu telefone");
+telefone = Console.ReadLine();
+
+Console.WriteLine("Digite seu e-mail");
+email = Console.ReadLine();
+
+Console.WriteLine("Digite sua descrição");
+descricao = Console.ReadLine();
+
+
+Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+Console.WriteLine("X                                        X");
+Console.WriteLine("X               "+ nome +"               X");
+Console.WriteLine("X            "+ profissao +"             X");
+Console.WriteLine("X             "+ telefone +"             X");
+Console.WriteLine("X              "+ email +"               X");
+Console.WriteLine("X            "+ descricao +"             X");
+Console.WriteLine("X                                        X");
+Console.WriteLine("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
